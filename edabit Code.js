4771fca61b92c7238ee5a9f1751c90626ba1b007
Sophie Hanson <116099929+SophieHanson19@users.noreply.github.com>
2023-04-21 15:54:31 +0100

    Add files via upload

---
+++ edabit Code.js
@@ -0,0 +1,8 @@
+function timeForMilkAndCookies(date) {
+  var day = date.getDate();
+  var month = date.getMonth();
+  
+  if(day == 24 && month == 11) {
+    return true
+  } else {
+    return false
